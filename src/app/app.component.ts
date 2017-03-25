8ccea0a16fa3d42725546b04bc0d6d1ff62716fb
Tidiane SIDIBE <stidiovip@yahoo.fr>
2017-03-25 21:48:50 +0100

    Fixing new contact fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,7 @@ export class AppComponent {
 
     // add, only if data are OK !
     if ( this.newContactInfo.name != "" && this.newContactInfo.address != "" && this.newContactInfo.phone != ""){
-      this.contacts.push(new Contact(this.contacts.length + 1, this.newContactInfo.name,  this.newContactInfo.name,  this.newContactInfo.name));
+      this.contacts.push(new Contact(this.contacts.length + 1, this.newContactInfo.name,  this.newContactInfo.address,  this.newContactInfo.phone));
 
         // clean
         this.newContactInfo = {
